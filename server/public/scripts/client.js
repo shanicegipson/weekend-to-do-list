3b0177f3ba702919d285230c0ae03eaa02a82c95
Shanice Gipson <shanicegipson13@gmail.com>
2019-08-17 11:00:29 -0500

    get data from server to DOM working, need to correct date format

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,18 +1,22 @@
 $(document).ready(init);
 
-function init(){
+function init() {
     console.log('She ready!')
 
-    $('.addButton').on('click', handleSubmit)
+    $('.addButton').on('click', handleSubmit);
+    $('.viewTask').on('click', '.js-check-btn', completedTask);
 }
 
+function completedTask(event) {
+    console.log('task complete');
+}
 
 function handleSubmit() {
     let taskToSend = {
         task: $('.toDo').val(),
         date: $('.dueDate').val(),
     };
-     
+
     console.log('In', taskToSend);
 
     postTask(taskToSend);
@@ -23,61 +27,61 @@ function handleSubmit() {
 }
 
 function postTask(taskToSend) {
-$.ajax({
-    type: 'POST',
-    url:'/task',
-    data: taskToSend,
-}).then(function (response) {
-    console.log('Response from server: ', response);
-    getTask();
-}).catch(function (error) {
-    console.log('Error in POST', error);
-    alert('Unable to add task at this time. Please try again later.');
-})
+    $.ajax({
+        type: 'POST',
+        url: '/task',
+        data: taskToSend,
+    }).then(function (response) {
+        console.log('Response from server: ', response);
+        getTask();
+    }).catch(function (error) {
+        console.log('Error in POST', error);
+        alert('Unable to add task at this time. Please try again later.');
+    })
 }
 
 function getTask() {
-$.ajax({
-    type:'GET',
-    url: '/task'
-}).then(function (response) {
-    console.log('Response from server: ', response);
-    // renderTask(response)
-}).catch(function (error) {
-    console.log('Error in GET', error);
-})
+    $.ajax({
+        type: 'GET',
+        url: '/task'
+    }).then(function (response) {
+        console.log('Response from server: ', response);
+        renderTask(response)
+    }).catch(function (error) {
+        console.log('Error in GET', error);
+    })
 }
 
-function renderText(koalas) {
-    $('.taskList').empty();
-  
-    for (let i = 0; i < koalas.length; i += 1) {
-      let koala = koalas[i];
-      // For each book, append a new row to our table
-  
-      let $tr = $('<tr></tr>');
-      $tr.data('koala', koala);
-      $tr.append(`<td>${koala.name}</td>`);
-      $tr.append(`<td>${koala.age}</td>`);
-      $tr.append(`<td>${koala.gender}</td>`);
-      $tr.append(`<td>${koala.ready_to_transfer}</td>`);
-      $tr.append(`<td>${koala.notes}</td>`);
-  
-      if(koala.ready_to_transfer =='N'){
-        $tr.append(`<td><button class="js-btn-markAsReady btn" data-id="${koala.id}">Ready for Transfer</button></td>`)
-      }else{
-        $tr.append(`<td>  </td>`);
-      }
-      
-      $tr.append(`<button class="js-btn-delete btn" data-id="${koala.id}">Delete</button></td>`);
-      $('#viewKoalas').append($tr);
+function renderTask(task) {
+
+    $('.viewTask').empty();
+
+    for (let i = 0; i < task.length; i += 1) {
+        let aTask = task[i];
+        let $tr = $('<tr></tr>');
+        $tr.data('task', aTask);
+
+        for (let aTask of task) {
+            let formatTime = moment(aTask.date).format('MMMM Do YYYY');
+
+            if (aTask.status == 'null') {
+                $tr.append(`<td><input class="js-check-btn" data-id="${aTask.id}"></input></td>`)
+            } else {
+                $tr.append(`<td>  </td>`);
+            }
+
+
+            $tr.append(`<td>${aTask.task}</td>`);
+            $tr.append(`<td>${formatTime}</td>`);
+
+            $tr.append(`<td>${changeStatus(aTask.status)}</td>`);
+
+            $tr.append(`<button class="js-btn-delete btn" data-id="${aTask.id}">Delete</button></td>`);
+            $('.viewTask').append($tr);
+        }
     }
-  }
+}
 
-
-function completedTask() {
-console.log('task complete');
-  }
 
 function changeStatus() {
     console.log('change of status');
